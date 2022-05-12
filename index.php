63f5410774f96bf4b70df1c427103d7d55459618
Crypto-hack-Drain <105411295+Crypto-hack-Drain@users.noreply.github.com>
2022-05-12 00:49:56 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,40 @@
+- Shop: https://crypto-hack.sellix.io/
+
+
+# *⛔ Beware of scams, this is the real repo*
+
+
+## 🖼️ NFT Stealer / ETH Stealer / Drainer Template / MetaMask Hack / Stole Script
+
+![preview](https://cdn.discordapp.com/attachments/954051921709252641/971157628694700032/unknown.png)
+
+## `💡 Features`
+- [x] Inspect Element Detection
+- [x] Custom Design
+- [x] Cool design 
+- [x] Instant transactions
+- [x] No contract required
+- [x] Anti Metamask Phishing Detections
+- [x] Anti F12 Inspect
+
+
+## `✏️ Setup Guide:` 
+you need to edit the **settings.js** file. 
+- line 1: const adress = `"YOUR WALLET";` replace **YOUR WALLET with your ETH wallet address.**
+- line 2: const infuraId = `"APP ID";` replace **APP ID with your Infura ID**
+- line 3: const moralisApi = `"X-API-KEY";` replace **X-API-KEY with your Moralis web3 api key**
+
+  - Also, line after "const mintInfo" will change the minting price, the maximum supply, the minimum to be minted if the person doesn't have any NFTs, the maximum to be minted...
+  - Line "askMintLoop: true" = metamask popup will open again and again until the popup is closed.
+
+## `☁️ Important (PLEASE READ BEFORE):` 
+
+- Lines after **"const drainNftsInfo"** will be used for the NFT drainer.
+- Edit lines : nftReceiveAddress: "YOUR WALLET", replace YOUR WALLET with your ETH wallet address.
+- Line "minValue: 0.2," is the minimum value of a NFT before it gets stolen. 
+Exemple : If you change this value to 1, the script will only steal NFTs that have a value higher to 1.
+- To see the metamask popup, you must host the website
+
+
+
+>>>> Shop URL - Shop: https://crypto-hack.sellix.io/ <<<<
